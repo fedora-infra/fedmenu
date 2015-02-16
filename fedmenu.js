f2547de1b8755aaaa5a4685f2083a36ee6279a92
Ralph Bean <rbean@redhat.com>
2015-02-15 20:33:49 -0500

    State on/off and scrollability.

--- fedmenu.js
+++ fedmenu.js
@@ -1,8 +1,13 @@
 $(document).ready(function() {
     $('body').append('<link href="fedmenu.css" rel="stylesheet">');
-    $('body').append('<div id="fedmenu-button">button</div>');
+    $('body').append('<div id="fedmenu-button"></div>');
     $('body').append('<div id="fedmenu-wrapper" class="fedmenu-hidden fedmenu-radial-shadow"></div>');
+    $("#fedmenu-wrapper").click(function() {
+        $('#fedmenu-button').removeClass('fedmenu-active');
+        $('#fedmenu-wrapper').addClass('fedmenu-hidden');
+    });
     $("#fedmenu-button").click(function() {
+        $('#fedmenu-button').addClass('fedmenu-active');
         $('#fedmenu-wrapper').removeClass('fedmenu-hidden');
     })
     console.log('hi');
