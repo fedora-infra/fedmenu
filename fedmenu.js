61ff380383909a19529af5bca872cacf8edd2b6f
Ralph Bean <rbean@redhat.com>
2015-03-02 16:11:40 -0500

    Expand options, preparing for user/package hints.

--- fedmenu.js
+++ fedmenu.js
@@ -1,21 +1,37 @@
 var fedmenu = function(options) { $(document).ready(function() {
     var defaults = {
+        'url': 'https://apps.fedoraproject.org/js/data.js',
+        'mimeType': undefined,  // Only needed for local development
+
         'position': 'bottom-left',
-        //'user': null,
-        //'package': null,
+
+        'user': null,
+        'package': null,
     }
     // Our options object is called 'o' for shorthand
     var o = $.extend({}, defaults, options || {});
 
+    var buttons = "";
+    if (o['user'] != null) buttons += '<div id="fedmenu-user-button"></div>';
+    if (o['package'] != null) buttons += '<div id="fedmenu-package-button"></div>';
+    buttons += '<div id="fedmenu-main-button"></div>';
+
     $('body').append('<link href="fedmenu.css" rel="stylesheet">');
-    $('body').append('<div id="fedmenu-button" class="fedmenu-' + o.position + '"></div>');
+    $('body').append(
+        '<div id="fedmenu-tray" class="fedmenu-' + o.position + '">' +
+        buttons + '</div>');
     $('body').append('<div id="fedmenu-wrapper"></div>');
 
     $('body').append('<div id="fedmenu-content"></div>');
     $('#fedmenu-content').append("<h1>Fedora Infrastructure</h1>");
     $.ajax({
-        url: 'https://apps.fedoraproject.org/js/data.js',
+        url: o.url,
+        mimeType: o.mimeType,
         dataType: 'script',
+        error: function(err) {
+            console.log('Error getting ' + o.url);
+            console.log(err);
+        },
         success: function(script) {
             $.each(json.children, function(i, child) {
                 var html =
@@ -34,11 +50,11 @@ var fedmenu = function(options) { $(document).ready(function() {
         },
     });
 
-    var main = '#fedmenu-wrapper,#fedmenu-button,#fedmenu-content';
+    var main = '#fedmenu-wrapper,#fedmenu-main-button,#fedmenu-content';
     var activate = function() { $(main).addClass('fedmenu-active'); };
     var deactivate = function() { $(main).removeClass('fedmenu-active'); };
 
-    $("#fedmenu-button").click(function() {
+    $("#fedmenu-main-button").click(function() {
         if ($(this).hasClass('fedmenu-active'))
             deactivate();
         else
